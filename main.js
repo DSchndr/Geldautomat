fdf87e4e91047cf0e2be0f7672be3e3fbc72601e
DSchndr <dennix1010@gmail.com>
2019-02-24 12:55:16 +0100

    add return to detectie

--- main.js
+++ main.js
@@ -183,4 +183,5 @@ function detectie(triggermessage = false) {
     if (navigator.appName == 'Microsoft Internet Explorer' || !!(navigator.userAgent.match(/Trident/) || navigator.userAgent.match(/rv:11/)) || (typeof $.browser !== "undefined" && $.browser.msie == 1) || triggermessage) {
         alert("Internet Explorer veraltet. Bitte Firefox, Google Chrome oder Opera benutzen.");
     }
+    return;
 }
